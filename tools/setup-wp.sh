d350ccec707852fce19136c50b1f5cf8d5031e43
Rakshan Shetty <shetty.raxx555@gmail.com>
2018-06-17 21:19:50 +0530

    :recycle: Installing wp via cli

--- tools/setup-wp.sh
+++ tools/setup-wp.sh
@@ -1,27 +1,26 @@
-if ! [ -x "wp" ]; then
+if [ -x "./tools/wp-cli.phar" ]; then
   echo "Setting up wordpress"
   cd ./tools
 elif ! [ -x "$(command -v wp)" ]; then
-    echo "Installing Wp-cli"
-    cd ./tools
-    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-    chmod +x wp-cli.phar
-    sudo mv wp-cli.phar wp
+  echo "Installing Wp-cli"
+  cd ./tools
+  wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+  chmod +x wp-cli.phar
 fi
 
 # Download wp setup
-./wp core download
+./wp-cli.phar core download
 
 # Launch the containers
-echo "Starting Docker containers..."
-docker-compose -f docker-compose-dev.yml up -d >/dev/null
+# echo "Starting Docker containers..."
+# docker-compose -f docker-compose-dev.yml up -d >/dev/null
 
 # Wait until the docker containers are setup properely
-echo -en $(status_message "Attempting to connect to wordpress...")
-until $(curl -L http://localhost:8080 -so - 2>&1 | grep -q "WordPress"); do
-    echo -n '.'
-    sleep 5
-done
-echo ''
+# echo "Attempting to connect to wordpress..."
+# until $(curl -L http://localhost:8080 -so - 2>&1 | grep -q "WordPress"); do
+#     echo -n '.'
+#     sleep 5
+# done
+# echo ''
 
-./wp core install
+./wp-cli.phar config create --skip-check --force
